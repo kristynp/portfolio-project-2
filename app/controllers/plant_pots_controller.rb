e4bebcae62dc4d4c85e1542b2d3490fc329e9184
github username <github email address>
2020-02-14 13:15:28 -0700

    added plant pot edit and show pages, added plant selection to new

--- app/controllers/plant_pots_controller.rb
+++ app/controllers/plant_pots_controller.rb
@@ -16,7 +16,32 @@ class PlantPotsController < ApplicationController
   end
 
   post '/plant_pots' do
-    @
+    if !params[:color].empty? && !params[:size].empty? && current_user != nil
+      @plant_pot = PlantPot.create(color: params[:color], size: params[:size])
+      @plant_pot.plant_id = params[:plant_id]
+      @plant_pot.save
+      redirect '/plant_pots'
+    else
+      redirect '/plant_pots/new'
+    end
+  end
+
+  get '/plant_pots/:id' do
+    if logged_in?
+      @plant_pot = PlantPot.find_by(id: params[:id])
+      erb :'/plant_pots/show'
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/plant_pots/:id/edit' do
+    if logged_in?
+      @plant_pot = PlantPot.find_by(id: params[:id])
+      erb :'/plant_pots/edit'
+    else
+      redirect '/plants'
+    end
   end
 
 end
