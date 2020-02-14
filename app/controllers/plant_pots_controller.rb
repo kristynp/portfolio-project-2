f0f2210da5bfc9c6b67fdfba6932cd0104a5cc18
github username <github email address>
2020-02-14 10:16:42 -0700

    added required attribute to all radio buttons, added plant_pot index and new routes and view

--- app/controllers/plant_pots_controller.rb
+++ app/controllers/plant_pots_controller.rb
@@ -1,3 +1,22 @@
-class PlantPotsController < Sinatra::Base
-  
+class PlantPotsController < ApplicationController
+  get '/plant_pots' do
+    erb :'/plant_pots/index'
+  end
+
+  get '/plant_pots/new' do
+    if logged_in?
+      erb :'/plant_pots/new'
+    else
+      redirect '/login'
+    end
+  end
+
+  post '/plant_pots/new' do
+    redirect '/plant_pots/new'
+  end
+
+  post '/plant_pots' do
+    @
+  end
+
 end
